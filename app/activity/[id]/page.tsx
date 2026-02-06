b7e9c07e2ecf4472b43e97d298845dea4a2263e3
윤효정 <0213yhj@naver.com>
2026-02-07 06:10:15 +0900

    fix search input width

--- app/activity/[id]/page.tsx
+++ app/activity/[id]/page.tsx
@@ -37,8 +37,8 @@ export default function ActivityDetailPage() {
         </header>
 
         {/* 메인 컨텐츠 */}
-        <div className="flex justify-center w-full px-4">
-          <div className="my-20 w-full max-w-5xl">
+        <div className="w-full px-4">
+          <div className="max-w-2xl mx-auto my-20">
             <SearchInput />
           </div>
         </div>
